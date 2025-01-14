f4c05421804ab388a39737992c81b17bcfeca19a
anitta <anittamariasiljo24@gmail.com>
2025-01-14 18:58:36 +0530

    Commit

--- incometax.py
+++ incometax.py
@@ -1,1 +1,12 @@
-input ("income")
+income = float(input("Enter your income: "))
+if income <= 250000:
+    tax = 0
+elif income <= 500000:
+    tax = 0.05 * (income - 250000)
+elif income <= 1000000:
+    tax = 12500 + 0.1 * (income - 500000)
+elif income <= 1500000:
+    tax = 75000 + 0.15 * (income - 1000000)
+else:
+    tax = 187500 + 0.2 * (income - 1500000)
+print("Income tax: ", tax)
