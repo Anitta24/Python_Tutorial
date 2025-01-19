c76ddd2f0b19cef8897442a8db28958c5ee6c5b8
anitta <anittamariasiljo24@gmail.com>
2025-01-19 23:36:03 +0530

    caesar_cipher

--- caser.py
+++ caser.py
@@ -5,7 +5,7 @@ for ch in message:
     ordinal_ch=ord(ch)
     ordinal_ch+=key
     if ordinal_ch>ord('z'):
-        ordinal_ch=ord('a')+(ordinal_ch-ord('z'))
+        ordinal_ch=ord('a')+(ordinal_ch-ord('z')-1)
     newch=chr(ordinal_ch)
     ciphertext+=newch
 print(ciphertext)
