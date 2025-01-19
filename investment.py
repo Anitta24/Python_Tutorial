c43a198242e90114c230560cc468e167979d53d0
anitta <anittamariasiljo24@gmail.com>
2025-01-19 12:41:50 +0530

    investment_report

--- investment.py
+++ investment.py
@@ -1,9 +1,9 @@
-initial_investment=float(input("Enter the initial investment:"))
-annual_intrest_rate=float(input("Enter the annual intrest rate(in %):"))
+initial_investment = float(input("Enter the initial investment:"))
+annual_intrest_rate = float(input("Enter the annual intrest rate(in %):"))
 year=int(input("Enter the number of years:"))
-annual_intrest_rate=annual_intrest_rate/100
+annual_intrest_rate = annual_intrest_rate/100
 print("\nYear\tAmount")
 amount=initial_investment
 for year in range(1,year+1):
     amount+=amount*annual_intrest_rate
-    print(f"{year}\t{amount:.2f}")
+    print(year,"\t","%.2f" % amount)
