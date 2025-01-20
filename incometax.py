20268e5a7325e62aa0b25a334f8994e9ca41c1ef
anitta <anittamariasiljo24@gmail.com>
2025-01-20 21:47:19 +0530

    income_tax

--- incometax.py
+++ incometax.py
@@ -1,12 +1,12 @@
 income = float(input("Enter your income:  "))
-if income <= 250000:
+if income <= 300000:
     tax = 0
-elif income <= 500000:
-    tax = 0.05 * (income - 250000)
-elif income <= 1000000:
-    tax = 12500 + 0.1 * (income - 500000)
-elif income <= 1500000:
-    tax = 75000 + 0.15 * (income - 1000000)
+elif income <= 600000:
+    tax = 0.05 * (income - 300000)
+elif income <= 900000:
+    tax = 12500 + 0.1 * (income - 600000)
+elif income <= 1200000:
+    tax = 75000 + 0.15 * (income - 1200000)
 else:
     tax = 187500 + 0.2 * (income - 1500000)
 print("Income tax: ", tax)
